240430c68de7465eb131f4e73850e1ee653b627a
ionut <ionut.s_94@yahoo.com>
2026-02-05 21:08:04 +0200

    WIP

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -9,7 +9,7 @@ export const Searchbar: React.FC<Props> = ({ }) => {
   return (
     <div className="px-4 py-1 rounded-lg border border-sidebar-primary shadow-sm bg-card-secondary flex items-center justify-center w-[400px] text-color-secondary">
       <Search />
-      <Input className="outline-none border-none shadow-none" placeholder="Cauta contracte, clienti, sabloane" />
+      <Input className="outline-none border-none shadow-none" placeholder="Search for clients, contracts or templates" />
     </div>
   )
 }
