58fa4f3d602d0df9d92274c05379cf290e0873af
ionut <ionut.s_94@yahoo.com>
2026-03-29 22:21:55 +0300

    WIP

--- src/app/c/view-contract/page.tsx
+++ src/app/c/view-contract/page.tsx
@@ -2,6 +2,7 @@ import { CommentsSection } from "@/app/view-contract/components/comments-section
 import { ContractContentView } from "@/app/view-contract/components/contract-content-view"
 import { ContractViewedTracker } from "@/app/view-contract/components/contract-viewed-tracker"
 import { PageWidth } from "@/components/layout"
+import { Card } from "@/components/ui/card"
 import { ViewContractPageHeader } from "@/components/view-contract-page-header"
 import { withSafeService } from "@/lib/services-utils/with-safe-service"
 import AuditService from "@/services/audit"
@@ -39,23 +40,30 @@ export default async function CompnayViewContract({ searchParams }: Props) {
   // TODO: Error handling for contractError, commentsError, auditLogError
 
   return (
-    <main className="bg-app">
+    <main className="flex bg-slate-200 flex-col min-h-screen">
       <ViewContractPageHeader
         contract={contractData} auditLog={auditLogData || []}
       />
-      <PageWidth className="flex flex-1 gap-4 justify-between py-4 shadow-sm">
-        <ContractContentView
-          isSender={true}
-          contract={contractData}
-        />
-      </PageWidth>
-      <PageWidth>
-        <CommentsSection
-          user={contractData.owner}
-          comments={commentsData || []}
-          contract={contractData}
-          isSender={true}
-        />
+      <PageWidth className="flex flex-1 h-full gap-4 justify-between py-4 shadow-sm">
+        <div className="w-full lg:w-65/100">
+          <ContractContentView
+            isSender={true}
+            contract={contractData}
+          />
+        </div>
+
+        <div className="lg:w-35/100 self-start h-[85vh] rounded-lg min-h-0 overflow-y-auto">
+          <Card className="flex-1 min-h-0 rounded-md px-4 py-0 overflow-y-auto">
+            <div className="h-full min-h-0">
+              <CommentsSection
+                user={contractData.owner}
+                comments={commentsData || []}
+                contract={contractData}
+                isSender={true}
+              />
+            </div>
+          </Card>
+        </div>
       </PageWidth>
       <ContractViewedTracker
         actorType="SENDER"
